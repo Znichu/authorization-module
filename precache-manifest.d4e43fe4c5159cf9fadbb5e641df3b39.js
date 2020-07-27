3cdd10a23b85355404bfdfd698c575844185e094
Znichu <znichu1989@gmail.com>
2020-07-28 01:33:41 +0300

    Updates

---
+++ precache-manifest.d4e43fe4c5159cf9fadbb5e641df3b39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f76b40f7632f9ffa2904c05a8aadce6",
+    "url": "/authorization-module/index.html"
+  },
+  {
+    "revision": "5cc4a021b059b8b06179",
+    "url": "/authorization-module/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "8be59529c13bf34d9657",
+    "url": "/authorization-module/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "5cc4a021b059b8b06179",
+    "url": "/authorization-module/static/js/2.c82302db.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/authorization-module/static/js/2.c82302db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8be59529c13bf34d9657",
+    "url": "/authorization-module/static/js/main.e8d780dd.chunk.js"
+  },
+  {
+    "revision": "4015192bd132765b58a1",
+    "url": "/authorization-module/static/js/runtime-main.4849c254.js"
+  }
+]);

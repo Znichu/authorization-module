a917c019f4174a754135a3b91a8401a88ad3a03a
Home PC <sindzi0007@yandex.ru>
2020-07-29 20:21:01 +0300

    added jsx, validations for forms, connected react-hook-form and hookform.

--- src/components/Sign-In/SignIn.tsx
+++ src/components/Sign-In/SignIn.tsx
@@ -1,10 +1,54 @@
 import * as React from 'react';
+import {useForm} from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers";
+import * as yup from "yup";
+import {Checkbox, Alert} from "antd";
+
 
 type Props = {};
+
+type  LoginData = {
+    login: string;
+    password: string;
+}
+
+const schema = yup.object().shape({
+    login: yup.string().required().min(2),
+    password: yup.number().positive().integer().required().min(8),
+});
+
 export const SignIn = (props: Props) => {
+
+    const {register, handleSubmit, errors} = useForm<LoginData>({
+        resolver: yupResolver(schema)
+    });
+
+    const onSubmit = (data: LoginData) => console.log(data);
+
     return (
         <div>
-            <h3>SignIn</h3>
+            <div>
+                <h3>SignIn</h3>
+            </div>
+            <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div>
+                        <label>Login: </label>
+                        <input name="login" type="text" ref={register}/>
+                        {errors.login && <Alert message="Login is required" type="error" showIcon/>}
+                    </div>
+                    <div>
+                        <label>Password: </label>
+                        <input name="password" type="password" ref={register}/>
+                        {errors.password && (
+                            <Alert message="Password is required" type="error" showIcon/>)}
+                    </div>
+                    <div>
+                        <Checkbox>Remember Me</Checkbox>
+                        <button title="Enter" onClick={handleSubmit(onSubmit)}>Enter</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };

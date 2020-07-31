ce206c10e61e845b95747de393f3ae8f0467b52c
Xotonori <xotonori29011991@gmail.com>
2020-07-31 22:11:33 +0300

    created register page, register-reducer and registerApi

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,1 +1,15 @@
-export {}
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import thunk from "redux-thunk";
+import {registerReducer} from "./register-reducer";
+
+const rootReducer = combineReducers({
+//reducers
+    register: registerReducer
+});
+
+export type AppStateType = ReturnType<typeof rootReducer>
+
+// @ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+
+export let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));

2705e847ef82cc0280683b37b30f07b5cd82a7b1
Sergey Neplashov <sergeyznich@gmail.com>
2020-08-16 00:51:04 +0300

    Update Editor.tsx
    added possibility save textarea value in text file on local disk

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import style from './Editor.module.css'
 
 import ReactPlayer from 'react-player'
@@ -8,14 +8,37 @@ import {CloudDownloadOutlined, SaveOutlined, CloudUploadOutlined} from '@ant-des
 
 
 export const Editor = () => {
+
+    const [value, setValue] = useState('')
+
+    const onChange = ({ target: { value } }:any ) => {
+        setValue(value)
+    }
+    const saveTextFile = (fileName: string, value: string) => {
+        const link = document.createElement("a");
+
+        // создаём Blob из строк которые находятся в стэйте
+        const file = new Blob([value], {type: 'text/plain'});
+
+        //URL.createObjectURL берёт Blob и создаёт уникальный URL для него в формате blob:<origin>/<uuid>.
+        //пример: blob:https://javascript.info/1e67e00e-860d-40a5-89ae-6ab0cbee6273
+        link.href = URL.createObjectURL(file);
+        link.download = fileName;
+        link.click();
+        //удаляет внутреннюю ссылку на объект
+        URL.revokeObjectURL(link.href)
+    }
+
     return (
         <div className={style.wrapper}>
             <div className={style.textEditor}>
                 <TextArea
+                    value={value}
                     autoSize={{minRows: 3, maxRows: 5}}
+                    onChange={onChange}
                 />
                 <div className={style.btnGroup}>
-                    <Button type="primary" icon={<SaveOutlined />} >Save</Button>
+                    <Button onClick={() => saveTextFile('Text.txt', value + `\r\n`)} type="primary" icon={<SaveOutlined />} >Save</Button>
                     <Button type="primary" icon={<CloudUploadOutlined />} >Send</Button>
                     <Button type="primary" icon={<CloudDownloadOutlined />} >Download</Button>
                 </div>

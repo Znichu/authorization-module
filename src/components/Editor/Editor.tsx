e87f4654cada70b3e988378cae17bbc237bf94c9
Znichu <znichu1989@gmail.com>
2020-08-15 23:30:29 +0300

    added jsx for text editor

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -2,12 +2,25 @@ import React from "react";
 import style from './Editor.module.css'
 
 import ReactPlayer from 'react-player'
+import TextArea from "antd/es/input/TextArea";
+import {Button} from "antd";
+import {CloudDownloadOutlined, SaveOutlined, CloudUploadOutlined} from '@ant-design/icons';
 
 
 export const Editor = () => {
     return (
         <div className={style.wrapper}>
-            <div className={style.youtubePlayer}>
+            <div className={style.textEditor}>
+                <TextArea
+                    autoSize={{minRows: 3, maxRows: 5}}
+                />
+                <div className={style.btnGroup}>
+                    <Button type="primary" icon={<SaveOutlined />} >Save</Button>
+                    <Button type="primary" icon={<CloudUploadOutlined />} >Send</Button>
+                    <Button type="primary" icon={<CloudDownloadOutlined />} >Download</Button>
+                </div>
+            </div>
+            <div className={style.player}>
                 <ReactPlayer
                     url={'https://www.youtube.com/watch?v=pnkuI8KXW_8&t=43s'}
                     controls
